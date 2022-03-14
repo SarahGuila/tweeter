1f92fb8616a700b5223f5647c793ff032268f50e
Sarah Guila <sarahguila26@gmail.com>
2022-03-14 13:43:27 +0200

    Added database

---
+++ database/db.js
@@ -0,0 +1,76 @@
+const { v4 } = require("uuid");
+
+module.exports.db = [
+    {
+        username: "Jo",
+        password: "123",
+        nickname: "Jojo",
+        avatar: "https://image.shutterstock.com/image-vector/avatar-man-icon-symbol-simple-260nw-1701935266.jpg",
+        followers: ["Johana"],
+        tweets: [
+            {
+                id: v4(),
+                text: "Hello this is my first tweet!",
+                published: new Date(),
+                likes: ["Johana", "Michal"],
+                comments: [{
+                    username: "Jenny",
+                    text: "How , so smart!",
+                    published: new Date()
+                }
+            ]
+            },
+            {
+                id: v4(),
+                text: "Hello this is my second tweet!",
+                published: new Date(),
+                likes: ["Jenny"],
+                comments: [{
+                    username: "Johana",
+                    text: "intresting!",
+                    published: new Date()
+                }]
+            }
+        ]
+    },
+    {
+        username: "Johana",
+        password: "123",
+        nickname: "Johana",
+        avatar: "https://image.shutterstock.com/image-vector/avatar-man-icon-symbol-simple-260nw-1701935266.jpg",
+        followers: ["Jo"],
+        tweets: [
+            {
+                id: v4(),
+                text: "Hello everybody!",
+                published: new Date(),
+                likes: ["Jo"],
+                comments: [{
+                    username: "Jenny",
+                    text: "How , so smart!",
+                    published: new Date()
+                }
+            ]
+            },
+            {
+                id: v4(),
+                text: "Hello everbody again!",
+                published: new Date(),
+                likes: ["Jenny"],
+                comments: [{
+                    username: "Jo",
+                    text: "intresting!",
+                    published: new Date()
+                }]
+            }
+        ]
+    },
+    {
+        username: "Jenny",
+        password: "123",
+        nickname: "Jen",
+        avatar: "https://image.shutterstock.com/image-vector/avatar-man-icon-symbol-simple-260nw-1701935266.jpg",
+        followers: [],
+        tweets: []
+    }
+]
