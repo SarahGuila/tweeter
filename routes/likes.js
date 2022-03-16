3849a1ca0940574f03de3ba269005eddf5a60a66
Sarah Guila <sarahguila26@gmail.com>
2022-03-16 21:05:58 +0200

    likes rout done

--- routes/likes.js
+++ routes/likes.js
@@ -1,7 +1,20 @@
+const { db } = require('../database/db')
+const { onlyLogged } = require('../helpers/onlyLogged')
+
 const router = require('express').Router()
 
-router.post('/', (req, res) => {
-    
+router.post('/', onlyLogged, (req, res) => {
+    const { author, tweet_id } = req.body
+    const user = db.find(ur => ur.username == author)
+    const tweet = user.tweets.find(t => t.id == tweet_id)
+
+    if (tweet.likes?.includes(req.user.username)) {
+        tweet.likes = tweet.likes.filter(t => t != req.user.username)
+        res.send({ msg: 'Ho why ?' })
+    } else {
+        tweet.likes.push(req.user.username)
+        res.send({ msg: 'Great you add a like' })
+    }
 })
 
 module.exports = router
