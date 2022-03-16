7cb0b90ec21eff81f3b9ef2e0dc2ac63d3b2e7d7
Sarah Guila <sarahguila26@gmail.com>
2022-03-16 16:20:56 +0200

    everyOne middleware done

---
+++ helpers/everyone.js
@@ -0,0 +1,15 @@
+const jsonwebtoken = require("jsonwebtoken")
+
+module.exports.everyone = function(req, res, next){
+    try {
+        const dec = jsonwebtoken.decode(req.cookies.sid)
+        if (dec) {
+        req.user = dec            
+        } else {
+            req.user = false
+        }
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+}
