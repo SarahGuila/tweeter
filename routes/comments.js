819912181403cd80b1032bfa7af8d073b0830d0e
Sarah Guila <sarahguila26@gmail.com>
2022-03-16 20:49:49 +0200

    comments rout done

--- routes/comments.js
+++ routes/comments.js
@@ -1,11 +1,33 @@
+const { db } = require('../database/db')
+const { onlyLogged } = require('../helpers/onlyLogged')
+
 const router = require('express').Router()
 
 
-router.get('/:tweet_id', (req, res) => {
-    
+router.get('/:author/:tweet_id', (req, res) => {
+    const { author, tweet_id } = req.params
+    if (!author || !tweet_id) {
+        res.status(400).send({ msg: 'missing some info' })
+    }
+    const user = db.find(ur => ur.username == author)
+    const tweet = user.tweets.find(t => t.id == tweet_id)
+    res.send(tweet.comments)
 })
-router.post('/', (req, res) => {
-    
+
+router.post('/', onlyLogged, (req, res) => {
+    const { author, text, tweet_id } = req.body
+
+    if (!author || !tweet_id || !text) {
+        return res.status(400).send({ msg: 'missing some info' })
+    }
+    const user = db.find(ur => ur.username == author)
+    const tweet = user.tweets.find(t => t.id == tweet_id)
+    tweet.comments.push({
+        text,
+        published: new Date(),
+        username: req.user.username
+    })
+    res.send({ msg: 'your comment will be published soon!' })
 })
 
 module.exports = router
